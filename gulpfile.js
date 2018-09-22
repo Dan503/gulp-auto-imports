b9e14e435fac2c7cc09bde4463f71c79959ddc14
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-09-22 12:39:04 +1000

    Starting code off using gulp-concat as a base
    gulp-concat is kind of related to what I want to do in the sense that I want to take lots of files and merge them all down into one.

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,13 @@ var fileLoader = require('./index');
 gulp.task('default', function(){
 	return gulp.src([
 		'./test/scss/scss-input/**/*.scss',
-		'./other-test/scss/**/*.scss'
+		'./other-test-folder/scss/**/*.scss'
 	])
-		.pipe(fileLoader({
-			format: '@import "$path";',
-			//needed so that paths are able to be generated effectively
-			dest: './test/scss/file-loader.scss',
-		}))
+		// .pipe(fileLoader({
+		// 	format: '@import "$path";',
+		// 	//needed so that paths are able to be generated effectively
+		// 	outputFile: 'file-loader.scss',
+		// }))
+		.pipe(fileLoader('all.scss'))
 		.pipe(gulp.dest('./test/scss'))
 })
