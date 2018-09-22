292d932cb151cce312393d17a193865019b69284
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-09-22 19:46:21 +1000

    Moving "dest" into a variable

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,18 @@ var fileLoader = require('./index');
 
 
 gulp.task('default', function(){
+
+	var dest = 'test/scss';
+
 	return gulp.src([
 		'./test/scss/scss-input/**/*.scss',
 		'./other-test-folder/scss/**/*.scss'
 	])
-		.pipe(fileLoader('all.scss', {
+		.pipe(fileLoader({
 			format: '@import "$path";',
 			//needed so that relative paths are able to be generated properly
-			dest: 'test/scss',
-			outputFile: 'file-loader.scss'
+			dest: dest,
+			fileName: 'file-loader.scss'
 		}))
-		.pipe(gulp.dest('test/scss'))
+		.pipe(gulp.dest(dest))
 })
