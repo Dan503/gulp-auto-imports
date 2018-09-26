de0451dacaafc8d0ffe569d06550928f322a71d2
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-09-26 20:45:17 +1000

    Making sure templates don't break when order retention is enabled

--- test/js/file-loader.js
+++ test/js/file-loader.js
@@ -1,12 +1,12 @@
 
-import one from "./js-input/one.js";
+import one from "../../other-test-folder/js/one.js";
+import one_1 from "./js-input/one.js";
 import two from "./js-input/two.js";
-import one_1 from "../../other-test-folder/js/one.js";
 import two_1 from "../../other-test-folder/js/two.js";
 
 export default function(){
   one();
-  two();
   one_1();
+  two();
   two_1();
 }
