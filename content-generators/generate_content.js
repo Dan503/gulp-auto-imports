792adc1a545970c749512625e28b58d3ab12e547
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-09-27 20:56:28 +1000

    Updating generate_content function with header and footer handling

--- content-generators/generate_content.js
+++ content-generators/generate_content.js
@@ -5,7 +5,13 @@ var format_paths = require('../formatters/format_paths');
 var format_template = require('../formatters/format_template');
 
 module.exports = function generate_content ({ pathsArray, opt }) {
-	return isString(opt.format) ?
+
+	var output = isString(opt.format) ?
 		format_paths(pathsArray, opt.format) :
 		format_template(pathsArray, opt.format, opt.template);
+
+	var header = opt.header ? `\n${opt.header}\n` : '\n';
+	var footer = opt.footer ? `\n${opt.footer}\n` : '\n';
+
+	return header + output + footer;
 }
