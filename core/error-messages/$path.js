fe89c4e49d1ee77620e0a4d13eff7bf6d8592bf7
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-09-28 22:22:27 +1000

    Moving $path error message to error messages folder and making it pretty

---
+++ core/error-messages/$path.js
@@ -0,0 +1,5 @@
+
+var c = require('chalk');
+
+module.exports =
+`"${c.cyan('$path')}" can only be declared once per format rule.`;
--- core/formatters/formatPath.js
+++ core/formatters/formatPath.js
@@ -1,9 +1,10 @@
 
 var err = require('../helpers/err');
+var path_error = require('../error-messages/$path');
 
 module.exports = function formatPath (filePath, format) {
   var regEx = /\$path/g;
   var pathCount = (format.match(regEx) || []).length;
-  err(pathCount > 1, '"$path" can only be declared once per format rule');
+  err(pathCount > 1, path_error);
   return format.replace(regEx, filePath);
 }
