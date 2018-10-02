8ce215b5905fe0e3baad4de7accc32ae3687cf38
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-10-02 23:02:44 +1000

    Removing "relative" module dependency
    The native path.relative() function does exactly what I want. There is no need to add the extra "relative" module dependency to the project.

--- core/content-generators/get_relative_path.js
+++ core/content-generators/get_relative_path.js
@@ -1,5 +1,5 @@
 
-var relative = require('relative');
+var path = require('path');
 
 var slash = require('../helpers/slash');
 var join = require('../helpers/join');
@@ -7,7 +7,7 @@ var join = require('../helpers/join');
 module.exports = function get_relative_path(file, dest) {
   var from = slash( join([file.cwd, dest]) );
   var to = slash(file.history[0]);
-  var relativePath = slash( relative(from, to) );
+  var relativePath = slash( path.relative(from, to) );
   var relFixDots = relativePath.replace(/^(\.\.?)/, '$1/');
   var relDotSlash = relFixDots.replace(/^([^.\/])/, './$1');
   var relDupesRemoved = relDotSlash.replace(/\/\//g, '/');
