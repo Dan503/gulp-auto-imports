fa7b3e4fd9f64060ffb4d521a5964d45491affb0
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-10-06 16:42:53 +1000

    Order retention support progress
    It's not working yet. not sure why.

--- core/content-generators/get_ordered_paths.js
+++ core/content-generators/get_ordered_paths.js
@@ -1,15 +1,49 @@
 
+var path = require('path');
+
+var isString = require('../helpers/isString');
+
 var get_paths_from_string = require('./get_paths_from_string');
 
-module.exports = function get_ordered_paths ({ oldContent, newPaths }) {
+var replace_slash = (stringPath, replacement) => stringPath.replace(/[\/\\]+/g, replacement);
+
+module.exports = function get_ordered_paths ({ oldContent, newPaths, opt }) {
+
+	if (!isString(opt.format)) {
+		var formatNames = Object.keys(opt.format);
+		var formatValues = Object.values(opt.format);
+		var formatWithPath = formatValues.filter(value => /\$([_\\]*2)?(path|absolute)/.test(value));
+	}
 
 	var contains = (array, value) => array.indexOf(value) > -1;
 	var oldOrderedPaths = get_paths_from_string(oldContent);
 
+	console.log({oldOrderedPaths});
+	console.log({newPaths});
+
 	var newOrderedPaths = [];
-	newPaths.forEach((path) => {
-		if (!contains(oldOrderedPaths, path)) {
-			newOrderedPaths.push(path);
+	newPaths.forEach((filePath) => {
+		var replace_absolute_slash = replacement => replace_slash(path.resolve(opt.dest, filePath), replacement);
+
+		var pathTypes = [
+			//relative
+			replace_slash(filePath, '/'),
+			replace_slash(filePath, '\\'),
+			replace_slash(filePath, '\\\\'),
+
+			//absolute
+			replace_absolute_slash('/'),
+			replace_absolute_slash('\\'),
+			replace_absolute_slash('\\\\'),
+		];
+
+		console.log({filePath});
+		console.log({pathTypes});
+
+		var isFound = pathTypes.some(testPath => contains(oldOrderedPaths, testPath));
+
+		if (!isFound) {
+			newOrderedPaths.push(filePath);
 		}
 	});
 
--- core/content-generators/order_content.js
+++ core/content-generators/order_content.js
@@ -3,6 +3,6 @@ var get_ordered_paths = require('./get_ordered_paths');
 var generate_content = require('./generate_content');
 
 module.exports = function order_content ({ oldContent, newPaths, opt }) {
-	var orderedPaths = get_ordered_paths({ oldContent, newPaths });
+	var orderedPaths = get_ordered_paths({ oldContent, newPaths, opt });
 	return generate_content({ pathsArray: orderedPaths, opt});
 }
