5678ac1469dc29c65901faee90a70d39e9f24b2a
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-10-06 14:56:09 +1000

    Adding absolute path support to path finder regex
    The old regex only worked for relative paths. This new regex adds support for absolute paths. I checked how Mac and Linux do their absolute paths so that I could ensure that it worked on those platforms as well.

--- core/content-generators/get_paths_from_string.js
+++ core/content-generators/get_paths_from_string.js
@@ -2,7 +2,7 @@
 var remove_dupes = require('../helpers/remove_dupes');
 
 module.exports = function get_paths_from_string(string){
-	var pathRegEx = /\.\.?\/[^\n"?:*<>|]+\.[A-z0-9]+/g;
+	var pathRegEx = /(\.\.?|[A-Z]\:)?[\/\\][^\n"?:*<>|]+\.[A-z0-9]+/g;
 	var paths = string.match(pathRegEx);
 	return remove_dupes(paths);
 }
