e0c1656be9e94d46b95d3568a46591c9fbe50c92
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-11-12 22:33:36 +1100

    #8 - Placing item at closest matching index

--- core/content-generators/get_ordered_paths.js
+++ core/content-generators/get_ordered_paths.js
@@ -6,18 +6,43 @@ module.exports = function get_ordered_paths ({ oldContent, newPaths }) {
 	var contains = (array, value) => array.indexOf(value) > -1;
 	var oldOrderedPaths = get_paths_from_string(oldContent);
 
-	var newOrderedPaths = [];
+	var newOrderedPaths = [].concat(oldOrderedPaths);
 	newPaths.forEach((path) => {
 		if (!contains(oldOrderedPaths, path)) {
-			newOrderedPaths.push(path);
+			var matchingIndex = get_highest_index_match(path, oldOrderedPaths);
+			newOrderedPaths.splice(matchingIndex + 1, 0, path);
 		}
 	});
 
-	var ammendedPaths = oldOrderedPaths.concat(newOrderedPaths);
-
-	var finalPaths = ammendedPaths.filter(path => {
+	var finalPaths = newOrderedPaths.filter(path => {
 		return contains(newPaths, path);
 	});
 
 	return finalPaths;
+}
+
+function get_highest_index_match(targetPath, comparisonArray) {
+	var lineScores = comparisonArray.map(comparisonPath => compare_paths(targetPath, comparisonPath));
+	var highestIndex = lineScores.reduce((highIndex, score, index) => {
+		if (highIndex <= score) {
+			highIndex = index;
+		}
+		return highIndex;
+	}, 0)
+
+	return highestIndex;
+}
+
+function compare_paths(targetPath, comparisonPath) {
+	var targetArray = targetPath.split('');
+	var comparisonArray = comparisonPath.split('');
+
+	var i = 0;
+	var compare = i => targetArray[i] == comparisonArray[i];
+	var isDefined = i => typeof targetArray[i] !== 'undefined';
+	while (isDefined(i) && compare(i)){
+		i++;
+	}
+
+	return i;
 }
