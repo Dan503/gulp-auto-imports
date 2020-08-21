a03bda010a91c6eff00d7b56abb1a71626528c08
Daniel Tonon <dan503.npm@gmail.com>
2020-08-22 00:14:30 +1000

    Creating new js export preset files

--- core/content-generators/preset-settings.js
+++ core/content-generators/preset-settings.js
@@ -4,7 +4,11 @@
 
 var presets = {
   es5: require("../../presets/es5.js"),
+  es5_default_exports: require("../../presets/es5_default_exports.js"),
+  es5_named_exports: require("../../presets/es5_named_exports.js"),
   es6: require("../../presets/es6.js"),
+  es6_default_exports: require("../../presets/es6_default_exports.js"),
+  es6_named_exports: require("../../presets/es6_named_exports.js"),
   jade: require("../../presets/jade.js"),
   pug: require("../../presets/pug.js"),
   sass: require("../../presets/sass.js"),
