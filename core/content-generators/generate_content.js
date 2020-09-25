b080e3cfda4e95f2fbee1a2cf4bc82eb177ff78c
Daniel Tonon <dan503.npm@gmail.com>
2020-09-25 21:03:52 +1000

    Running prettier on generate_content.js

--- core/content-generators/generate_content.js
+++ core/content-generators/generate_content.js
@@ -1,35 +1,35 @@
-var isString = require("../helpers/isString");
-var warn = require("../helpers/warn");
+var isString = require('../helpers/isString')
+var warn = require('../helpers/warn')
 
-var template_warning = require("../error-messages/warn-template");
+var template_warning = require('../error-messages/warn-template')
 
-var format_paths = require("../formatters/format_paths");
-var format_template = require("../formatters/format_template");
+var format_paths = require('../formatters/format_paths')
+var format_template = require('../formatters/format_template')
 
 module.exports = function generate_content({ pathsArray, opt }) {
-  var formatIsString = isString(opt.format);
+	var formatIsString = isString(opt.format)
 
-  warn(formatIsString && opt.template, template_warning);
+	warn(formatIsString && opt.template, template_warning)
 
-  var output = formatIsString
+	var output = formatIsString
 		? format_paths({
 				relativePaths: pathsArray,
 				format: opt.format,
 				formatReplace: opt.formatReplace,
-				options: opt
+				options: opt,
 		  })
 		: format_template({
-        relativePaths: pathsArray,
+				relativePaths: pathsArray,
 				formats: opt.format,
 				template: opt.template,
 				formatReplace: opt.formatReplace,
 				templateReplace: opt.templateReplace,
 		  })
 
-  var defaultHeadFoot = formatIsString ? "\n" : "";
+	var defaultHeadFoot = formatIsString ? '\n' : ''
 
-  var header = opt.header ? `${opt.header}\n` : defaultHeadFoot;
-  var footer = opt.footer ? `\n${opt.footer}` : defaultHeadFoot;
+	var header = opt.header ? `${opt.header}\n` : defaultHeadFoot
+	var footer = opt.footer ? `\n${opt.footer}` : defaultHeadFoot
 
-  return header + output + footer;
-};
+	return header + output + footer
+}
