b040617ca8b3815cd3c3be336a0c60d48ccbcbad
Daniel Tonon <dan503.npm@gmail.com>
2020-10-03 00:51:30 +1000

    Making more same-folder tests

--- gulp/same-folder-test.js
+++ gulp/same-folder-test.js
@@ -1,14 +1,43 @@
 var gulp = require('gulp')
 var createAutoImportTask = require('../createAutoImportTask')
 
-const [sameFolderTest] = createAutoImportTask({
-	sourceFolder: 'tests/same-folder-test',
+const sourceFolder = 'tests/same-folder-test'
+
+const preset = 'es6_default_exports'
+
+const [withFileExtensionSetting] = createAutoImportTask({
+	sourceFolder,
 	fileExtension: 'js',
+	taskPrefix: 'withExtension',
 	importerSettings: {
-		dest: 'tests/same-folder-test',
-		preset: 'es6',
-		fileName: `sameFolderOutput.test.js`,
+		dest: sourceFolder,
+		preset,
+		fileName: `output-withFileExtensionSetting.test.js`,
+	},
+})
+const [noFileExtensionSetting] = createAutoImportTask({
+	sourceFolder,
+	taskPrefix: 'noExtension',
+	importerSettings: {
+		dest: sourceFolder,
+		preset,
+		fileName: `output-noFileExtensionSetting.test.js`,
+	},
+})
+const [defaultFileName] = createAutoImportTask({
+	sourceFolder,
+	taskPrefix: 'defaultFileName',
+	importerSettings: {
+		dest: sourceFolder,
+		preset,
 	},
 })
 
-gulp.task('same-folder-test', gulp.parallel(sameFolderTest))
+gulp.task(
+	'same-folder-test',
+	gulp.series(
+		withFileExtensionSetting,
+		noFileExtensionSetting,
+		defaultFileName,
+	),
+)
